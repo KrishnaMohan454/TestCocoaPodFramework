12bb36dbb012dbffd733a0b9c2216e7650d1ac7d
IDM014 <IDM014@IDM012s-Mac-mini.local>
2018-12-05 10:43:20 +0530

    finished changes

---
+++ TestCocoaPodFramework/TestCocoaPodFramework/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  TestCocoaPodFramework
+//
+//  Created by IDM014 on 05/12/18.
+//  Copyright © 2018 e. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
---
+++ TestCocoaPodFramework/TestCocoaPodFramework/TestCocoaPodFramework.h
@@ -0,0 +1,19 @@
+//
+//  TestCocoaPodFramework.h
+//  TestCocoaPodFramework
+//
+//  Created by IDM014 on 05/12/18.
+//  Copyright © 2018 e. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <TestCocoaPodFramework/FirstViewController.h>
+//! Project version number for TestCocoaPodFramework.
+FOUNDATION_EXPORT double TestCocoaPodFrameworkVersionNumber;
+
+//! Project version string for TestCocoaPodFramework.
+FOUNDATION_EXPORT const unsigned char TestCocoaPodFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestCocoaPodFramework/PublicHeader.h>
+
+
