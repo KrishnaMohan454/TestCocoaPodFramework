ad431e744b014f1133b46598a57c43de7c186ff9
IDM014 <IDM014@IDM012s-Mac-mini.local>
2018-12-10 14:58:17 +0530

    Added another framework

---
+++ TestCocoaPodFramework/DocumentCaptureCocoaPods.framework/Headers/DataViewController.h
@@ -0,0 +1,15 @@
+//
+//  DataViewController.h
+//  DocumentCaptureCocoaPods
+//
+//  Created by IDM014 on 05/12/18.
+//  Copyright © 2018 e. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DataViewController : UIViewController
+
+-(void)printData;
+
+@end
---
+++ TestCocoaPodFramework/DocumentCaptureCocoaPods.framework/Headers/DocumentCaptureCocoaPods.h
@@ -0,0 +1,19 @@
+//
+//  DocumentCaptureCocoaPods.h
+//  DocumentCaptureCocoaPods
+//
+//  Created by IDM014 on 05/12/18.
+//  Copyright © 2018 e. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <DocumentCaptureCocoaPods/DataViewController.h>
+//! Project version number for DocumentCaptureCocoaPods.
+FOUNDATION_EXPORT double DocumentCaptureCocoaPodsVersionNumber;
+
+//! Project version string for DocumentCaptureCocoaPods.
+FOUNDATION_EXPORT const unsigned char DocumentCaptureCocoaPodsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DocumentCaptureCocoaPods/PublicHeader.h>
+
+
