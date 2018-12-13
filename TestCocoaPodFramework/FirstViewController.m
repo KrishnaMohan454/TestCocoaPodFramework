559aebe812017176eb2915804c1ace7d5efb2bb4
IDM014 <IDM014@IDM012s-Mac-mini.local>
2018-12-13 16:55:19 +0530

    changes made

---
+++ TestCocoaPodFramework/FirstViewController.m
@@ -0,0 +1,36 @@
+//
+//  FirstViewController.m
+//  TestCocoaPodFramework
+//
+//  Created by IDM014 on 05/12/18.
+//  Copyright © 2018 e. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+-(void)data{
+    NSLog(@"checking");
+    
+    DataViewController *data = [[DataViewController alloc]init];
+    [data printData];
+    
+    NSLog(@"frameworjk working on");
+}
+
+@end
